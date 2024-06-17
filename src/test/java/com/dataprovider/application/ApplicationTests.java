ca2909c306d802bcf3cb1ce45f1655d2dabc9ab1
Mattia Musarra Tubbi <mattiamusarra@MacBook-Air-di-Mattia.local>
2024-06-17 22:22:15 +0200

    first commit

---
+++ src/test/java/com/dataprovider/application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataprovider.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
